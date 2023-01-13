8d15f70aa3a23407e14acd9a70c05929d8d9c087
Danek309044 <daniel@benysek.cz>
2023-01-13 15:02:06 +0100

    ReAdded The website

---
+++ javascript/typed.js
@@ -0,0 +1,8 @@
+var typed = new Typed("#typing", {
+    strings: ["Discord Bot Developer", "Web Developer", "Player"],
+    typeSpeed: 100,
+    backSpeed: 10,
+    startDelay: 1000,
+    backDelay: 2500,
+    loop: true
+})
