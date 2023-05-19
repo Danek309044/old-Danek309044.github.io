5b7630a93ddf6a642e04094a271036e9215feedc
Danek309044 <daniel@benysek.cz>
2023-05-19 14:42:50 +0200

    Better font, redirects and new content

--- javascript/typed.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed("#typing", {
-    strings: ["Discord Bot Developer", "Web Developer", "Photographer", "Player"],
-    typeSpeed: 100,
-    backSpeed: 10,
-    startDelay: 1000,
-    backDelay: 2500,
-    loop: true
-})
